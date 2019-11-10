fe9e9425e67d3e2b9016027017125e07bd552317
JooJ <50976403+JoooooooooJ@users.noreply.github.com>
2019-11-09 22:55:27 -0300

    ?

--- src/FlinstonTrans/Media/Views/SystemScreenView.java
+++ src/FlinstonTrans/Media/Views/SystemScreenView.java
@@ -9,12 +9,12 @@ package FlinstonTrans.Media.Views;
  *
  * @author Cesar Oliveira
  */
-public class SysyemScreenView extends javax.swing.JFrame {
+public class SystemScreenView extends javax.swing.JFrame {
 
     /**
      * Creates new form TelaSistema
      */
-    public SysyemScreenView() {
+    public SystemScreenView() {
         initComponents();
     }
 
@@ -143,21 +143,23 @@ public class SysyemScreenView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SysyemScreenView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SystemScreenView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SysyemScreenView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SystemScreenView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SysyemScreenView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SystemScreenView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SysyemScreenView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SystemScreenView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SysyemScreenView().setVisible(true);
+                new SystemScreenView().setVisible(true);
             }
         });
     }
