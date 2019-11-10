336b6cb7a4940ce130ae713f4a4608675c147965
Cesar <cesar.augustooliveira2010@hotmail.com>
2019-11-10 19:06:31 -0300

    man a helo cara

--- src/FlinstonTrans/src/Classes/Conexao.java
+++ src/FlinstonTrans/src/Classes/Conexao.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FlinstonTrans.src.Classes;
 
-/**
- *
- * @author Cesar Oliveira
- */
 public class Conexao {
+    //Declaração de bibliotecas
     
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.SQLException;
+    import java.sql.Statement;
+    import javax.swing.JOptionPane;
+    import java.sql.ResultSet;
+    import javax.swing.plaf.RootPaneUI;
+    
+    public class Connection{
+        private ResultSet resSet = null;
+        private Connection cnc = null;
+        private Statment stmt = null;
+        private static String url = "jdbc:postgresql:"
+            + "//localhost:5432/postgres";
+        private static String usuario = "Cesar Oliveira";
+        private static String senha = "123"; 
+    
+    
+    }
 }
